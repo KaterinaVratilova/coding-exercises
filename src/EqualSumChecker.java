cbddce22bf64605edab90f654f6711b846a06148
Katerina Vratilova <katerina.vratilova@gmail.com>
2023-04-04 21:18:12 +0200

    Create method that returns true if the third parameter is equal to first and second.

---
+++ src/EqualSumChecker.java
@@ -0,0 +1,31 @@
+public class EqualSumChecker {
+
+  public static void main(String[] args) {
+    hasEqualSum(1, 1, 1);
+    hasEqualSum(1, 1, 2);
+    hasEqualSum(1, -1, 0);
+  }
+
+  public static boolean hasEqualSum(int a, int b, int c) {
+
+    if (a + b == c) {
+      return true;
+    } else return false;
+  }
+}
+
+/*
+Write a method hasEqualSum with 3 parameters of type int.
+
+The method should return boolean and it needs to return true if the sum of the first and second parameter is equal to the third parameter. Otherwise, return false.
+
+
+EXAMPLES OF INPUT/OUTPUT:
+
+* hasEqualSum(1, 1, 1);  should return false since 1 + 1 is not equal to 1
+
+* hasEqualSum(1, 1, 2);  should return true since 1 + 1 is equal to 2
+
+* hasEqualSum(1, -1, 0);  should return true since 1 + (-1) is 1 - 1 and is equal to 0
+
+* */
